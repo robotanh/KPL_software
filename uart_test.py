dc208d1a8723c5477f0a9f029ef80c75f229b0ce
robotanh <robotanh15@gmail.com>
2024-06-03 21:00:03 +0700

    fixing bug

--- uart_test.py
+++ uart_test.py
@@ -53,7 +53,7 @@ def main():
     port = '/dev/ttyS0'  # Replace with your serial port
     baud_rate = 9600
 
-    ser = serial.Serial(port, baud_rate, timeout=2)
+    ser = serial.Serial(port, baud_rate, timeout=3)
     time.sleep(2)  # Wait for the serial connection to initialize
 
     try:
@@ -63,18 +63,18 @@ def main():
             send_command(ser, 41)
             send_command(ser, 12)
             
-            time.sleep(1)  # Adjust timing as needed
+            time.sleep(3)  # Adjust timing as needed
 
             raw_data = ser.read(76)  # Read exactly 76 bytes of data
             print(raw_data)
-            # if len(raw_data) == 76:
-            #     parsed_data = parse_gas_pump_data(raw_data)
-            #     print("Parsed Data:", parsed_data)
-            #     # Here you can store the parsed data in a database or process it further
-            # else:
-            #     print("Incomplete data received. Length:", len(raw_data))
+            if len(raw_data) == 76:
+                parsed_data = parse_gas_pump_data(raw_data)
+                print("Parsed Data:", parsed_data)
+                # Here you can store the parsed data in a database or process it further
+            else:
+                print("Incomplete data received. Length:", len(raw_data))
             
-            time.sleep(10)  # Adjust the delay as needed for your application
+            time.sleep(5)  # Adjust the delay as needed for your application
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
