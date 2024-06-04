60ea4fa126659b72ed099cb2f5f755be75823765
cbct <clongnguynvn@gmail.com>
2024-06-04 12:37:50 +0700

    MySQL setup2

--- KPL_MySQL/KPL_MySQL_main.py
+++ KPL_MySQL/KPL_MySQL_main.py
@@ -2,11 +2,19 @@ from KPL_MySQL_header import *
 
 
 def main():
-    ma_lan_bom_array=[1,1,1,1,1,1,1]
-    for i in range (1,701):
-        rand_id_voi_bom=random.randint(1,7)
-        faking_data(rand_id_voi_bom,ma_lan_bom_array[rand_id_voi_bom-1])
-        ma_lan_bom_array[rand_id_voi_bom-1]+=1
+    # ma_lan_bom_array=[1,1,1,1,1,1,1]
+    # for i in range (1,701):
+    #     rand_id_voi_bom=random.randint(1,7)
+    #     faking_data(rand_id_voi_bom,ma_lan_bom_array[rand_id_voi_bom-1])
+    #     ma_lan_bom_array[rand_id_voi_bom-1]+=1
+    data={
+        "id_voi":1,
+        "ma_lan_bom":2,
+        "gia_ban":25000,
+        "tong_da_bom":5,
+        "tien_ban":125000
+    }
+    insert_gasbump_data(data)
 
 
 if __name__ == "__main__":
